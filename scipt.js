8814fc8c759640a0e231ea8768db0f5c18c3bbfb
Mitchell Bean <mitchtroybean@gmail.com>
2020-12-15 14:43:20 -0800

    restarted getting help from peer

--- scipt.js
+++ scipt.js
@@ -1,97 +1,101 @@
-var todoInput = document.querySelector("#todo-text”);
-var todoForm = document.querySelector("#todo-form”);
-var todoList = document.querySelector("#todo-list”);
-var todoCountSpan = document.querySelector("#todo-count”);
-var todos = ["Learn HTML”, "Learn CSS”, "Learn JavaScript”];
-// var todos = []
+    //TODO:create variables for questions
+    // Q"how many legs does a cat have?"
+    // Answers(1,2,4,3)
+
+    // Q"how many legs does a spider have?"
+    // answers(6,4,8,10)
+
+    // Q"how many legs does a human have?""
+    // answers(1,5,2,6)
+
+    // Q"how many legs does a fly have?
+    // A"2,4,8,6"
 
 
+    
+    //TODO:create countdown timer.
+   var count=60;
 
-renderTodos()
-function renderTodos(){
-   
-    
-    //TODO:Create a timer that stay contious on pag as quiz goes along changing n button click
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
 
-    //TODO8
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    todoCountSpan.textContent = todos.length;
-     for (var i = 0; i<todos.length; i++){
-        // todoList.appendChild(document.createElement(“li”))
-        var liEl = document.createElement ("li");
-        var textNode = document.createTextNode (todos[i]);
-        liEl.appendChild (textNode);
-        todoList.appendChild (liEl);
-        
-        liEl.setAttribute("data-index", i) 
-      }
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     //counter ended, do something here
+     return;
+  }
+
+  //Do code for showing the number of seconds here
 }
+    
+    //TODO:Create a title page with a button that begin coding quiz and timer begins counting down.
 
 
 
+    //TODO:Create the first form with elements to click for answers 
+    //TODO:When element clicked move onto next question with a display of last answer being correct or wrong
+    //TODO: When moved to next question the timer remains same counting down or subtracts 10 seconds and contiues.
+    //TODO:After questions answered remaining scroe displayed on final page. 
+    //TODO: User score can be saved with intials
 
-
-quizForm.addEventListener ("submit",function (event){
-    event.preventDefault ();
-    var todoText = todoInput.value;
-    console.log (todoText);
-    if (todoText === ""){
-    return;
-    }
-    todos.push (todoText);
-    renderTodos();
-    todoInput.value = "";
+    
+    //TODO:Create a Form 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 )
